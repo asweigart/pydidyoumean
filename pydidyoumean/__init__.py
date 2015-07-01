edfe24e0e45c481b995d9d2ec21f6459201141d4
Al Sweigart <asweigart@gmail.com>
2015-07-01 15:15:59 -0700

    Initial 0.1.0 version. (Though no docs yet.)

--- pydidyoumean/__init__.py
+++ pydidyoumean/__init__.py
@@ -1,16 +1,56 @@
-import os, sys
+import os
+import sys
+
+runningOnPython2 = sys.version_info[0] == 2
+
 
 '''Suggested usage:
 
 if FILE_NOT_FOUND:
-  pydidyoumean.printSuggestion(FILENAME) # if no suggested file is found, this prints nothing
+  pydidyoumean.printFileSuggestion(FILENAME) # if no suggested file is found in the cwd, this prints nothing
+
+if COMMAND_NOT_FOUND:
+  pydidyoumean.printSuggestion(CMD_NAME, LIST_OF_ALL_CMDS) # if no suggested command is found, this prints nothing
+
 '''
 
 def printFileSuggestion(filename, message='Did you mean %s?\n', folder='.', threshold=2, includeIdenticalFilename=False):
+  '''Prints the string message to display with the closest matching filename in
+  the folder. Prints nothing if there are no matches within the threshold.
+  Prints to sys.stdout.
+
+  Args:
+      filename (str) - The typo'd filename the user supplied.
+      message (str, optional) - The format of the suggestion message to print.
+        A %s conversion specifier will be replaced with the suggested name.
+        Default is 'Did you mean %s?\n'
+      folder (str, optional) - The folder with filenames the user could have
+        meant. Default is the . current folder.
+      threshold (int, optional) - A levenshtein edit distance above this
+        threshold will exclude a file from being suggested. Default is 2.
+      includeIdenticalFilename (bool, optional) - If True, a filename identical
+        to the filename arg will be included in the suggestions. Default is
+        False.'''
   sys.stdout.write(getFileSuggestion(filename, message, folder, threshold, includeIdenticalFilename))
 
 
 def getFileSuggestion(filename, message='Did you mean %s?\n', folder='.', threshold=2, includeIdenticalFilename=False):
+  '''Returns the string message to display with the closest matching
+  filename. Returns a blank string if there are no matches within the
+  threshold.
+
+  Args:
+      filename (str) - The typo'd filename the user supplied.
+      message (str, optional) - The format of the suggestion message to print.
+        A %s conversion specifier will be replaced with the suggested name.
+        Default is 'Did you mean %s?\n'
+      folder (str, optional) - The folder with filenames the user could have
+        meant. Default is the . current folder.
+      threshold (int, optional) - A levenshtein edit distance above this
+        threshold will exclude a file from being suggested. Default is 2.
+      includeIdenticalFilename (bool, optional) - If True, a filename identical
+        to the filename arg will be included in the suggestions. Default is
+        False.'''
   suggestedFile = suggestFile(filename, folder, threshold, includeIdenticalFilename)
   if suggestedFile is not None:
     return message % (suggestedFile)
@@ -19,15 +59,44 @@ def getFileSuggestion(filename, message='Did you mean %s?\n', folder='.', thresh
 
 
 def suggestFile(filename, folder='.', threshold=2, includeIdenticalFilename=False):
-  '''Returns the first'''
+  '''Returns the closest matching filename to the filenames in the folder. If
+  there are multiple files with the same edit distance, the one returned is
+  undefined. Returns None if there are no suggestions within the threshold.
+
+  Args:
+      filename (str) - The typo'd filename the user supplied.
+      folder (str, optional) - The folder with filenames the user could have
+        meant. Default is the . current folder.
+      threshold (int, optional) - A levenshtein edit distance above this
+        threshold will exclude a file from being suggested. Default is 2.
+      includeIdenticalFilename (bool, optional) - If True, a filename identical
+        to the filename arg will be included in the suggestions. Default is
+        False.'''
   try:
-    return suggestAllFiles(filename, folder, threshold, includeIdenticalFilename).__next__()
+    genObj = suggestAllFiles(filename, folder, threshold, includeIdenticalFilename)
+    if runningOnPython2:
+      return genObj.next() # Python 2 code
+    else:
+      return genObj.__next__() # Python 3 code
   except StopIteration:
     return None
 
 
 def suggestAllFiles(filename, folder='.', threshold=2, includeIdenticalFilename=False):
-  '''Returns the first. If there are multiple files with the same edit distance, they will be returned in an undefined order.'''
+  '''Returns all suggestions in possibleSuggestions that are within the
+  levenshtein edit distance of name, sorted by distance. If there are multiple
+  files with the same edit distance, they will be returned in an undefined
+  order.
+
+  Args:
+      filename (str) - The typo'd filename the user supplied.
+      folder (str, optional) - The folder with filenames the user could have
+        meant. Default is the . current folder.
+      threshold (int, optional) - A levenshtein edit distance above this
+        threshold will exclude a file from being suggested. Default is 2.
+      includeIdenticalFilename (bool, optional) - If True, a filename identical
+        to the filename arg will be included in the suggestions. Default is
+        False.'''
   filesAndDistances = [(f, levenshtein(filename, f)) for f in os.listdir(folder) if includeIdenticalFilename or f != filename]
   filesAndDistances = [x for x in filesAndDistances if x[1] <= threshold] # remove files over the threshold
   filesAndDistances.sort(key=lambda x: x[1]) # sort by comparing the levenshtein distance in x[1]
@@ -36,25 +105,108 @@ def suggestAllFiles(filename, folder='.', threshold=2, includeIdenticalFilename=
     yield fd[0]
 
 
-def printSuggestion(name, message='Did you mean %s?\n', possibleSuggestions=None, threshold=2, includeIdenticalFilename=False):
-  pass
+def printSuggestion(name, possibleSuggestions=None, message='Did you mean %s?\n', threshold=2, includeIdenticalName=False):
+  '''Prints the string message to display with the closest matching suggestion.
+  Prints nothing if there are no matches within the threshold. Prints to
+  sys.stdout.
+
+  Args:
+      name (str) - The typo'd name the user supplied.
+      possibleSuggestions (sequence of str, optional) - A sequence of strings
+        for all possible suggestions that the name could match to. This
+        function will pull suggestions from this list.
+      message (str, optional) - The format of the suggestion message to print.
+        A %s conversion specifier will be replaced with the suggested name.
+        Default is 'Did you mean %s?\n'
+      threshold (int, optional) - A levenshtein edit distance above this
+        threshold will exclude a name from being suggested. Default is 2.
+      includeIdenticalName (bool, optional) - If True, a name identical to
+        the name  arg will be included in the suggestions. Default is False.'''
+  sys.stdout.write(getSuggestion(name, possibleSuggestions, message, threshold, includeIdenticalName))
 
 
-def getSuggestion(filename, message='Did you mean %s?\n', possibleSuggestions=None, threshold=2, includeIdenticalFilename=False):
-  pass
+def getSuggestion(name, possibleSuggestions=None, message='Did you mean %s?\n', threshold=2, includeIdenticalName=False):
+  '''Returns the string message to display with the closest matching
+  suggestion. Returns a blank string if there are no matches within the
+  threshold.
+
+  Args:
+      name (str) - The typo'd name the user supplied.
+      possibleSuggestions (sequence of str, optional) - A sequence of strings
+        for all possible suggestions that the name could match to. This
+        function will pull suggestions from this list.
+      message (str, optional) - The format of the suggestion message to print.
+        A %s conversion specifier will be replaced with the suggested name.
+        Default is 'Did you mean %s?\n'
+      threshold (int, optional) - A levenshtein edit distance above this
+        threshold will exclude a name from being suggested. Default is 2.
+      includeIdenticalName (bool, optional) - If True, a name identical to
+        the name arg will be included in the suggestions. Default is False.'''
+  suggestedName = suggest(name, possibleSuggestions, threshold, includeIdenticalName)
+  if suggestedName is not None:
+    return message % (suggestedName)
+  else:
+    return ''
 
 
-def suggest(filename, possibleSuggestions=None, threshold=2, includeIdenticalFilename=False):
-  pass
+def suggest(name, possibleSuggestions=None, threshold=2, includeIdenticalName=False):
+  '''Returns the closest matching name to the suggestions in
+  possibleSuggestions. Pass a list of all possible matches for the
+  possibleSuggestions parameter. If there are multiple names with the same edit
+  distance, the one returned is undefined. Returns None if there are no
+  suggestions within the threshold.
+
+  Args:
+      name (str) - The typo'd name the user supplied.
+      possibleSuggestions (sequence of str, optional) - A sequence of strings
+        for all possible suggestions that the name could match to. This
+        function will pull suggestions from this list.
+      threshold (int, optional) - A levenshtein edit distance above this
+        threshold will exclude a name from being suggested. Default is 2.
+      includeIdenticalName (bool, optional) - If True, a name identical to
+        the name arg will be included in the suggestions. Default is False.'''
+  try:
+    genObj = suggestAll(name, possibleSuggestions, threshold, includeIdenticalName)
+    if runningOnPython2:
+      return genObj.next() # Python 2 code
+    else:
+      return genObj.__next__() # Python 3 code
+  except StopIteration:
+    return None
 
 
-def suggestAll(filename, possibleSuggestions=[], threshold=2, includeIdenticalFilename=False):
-  pass
+def suggestAll(name, possibleSuggestions=None, threshold=2, includeIdenticalName=False):
+  '''Returns all suggestions in possibleSuggestions that are within the
+  levenshtein edit distance of name, sorted by distance. If there are multiple
+  names with the same edit distance, they will be returned in an undefined
+  order.
+
+  Args:
+      name (str) - The typo'd name the user supplied.
+      possibleSuggestions (sequence of str, optional) - A sequence of strings
+        for all possible suggestions that the name could match to. This
+        function will pull suggestions from this list.
+      threshold (int, optional) - A levenshtein edit distance above this
+        threshold will exclude a name from being suggested. Default is 2.
+      includeIdenticalName (bool, optional) - If True, a name identical to
+        the name arg will be included in the suggestions. Default is False.'''
+  if possibleSuggestions is None:
+    possibleSuggestions = []
+  sugsAndDistances = [(n, levenshtein(name, n)) for n in possibleSuggestions if includeIdenticalName or n != name]
+  sugsAndDistances = [x for x in sugsAndDistances if x[1] <= threshold] # remove names over the threshold
+  sugsAndDistances.sort(key=lambda x: x[1]) # sort by the levenshtein distance in x[1]
+
+  for sd in sugsAndDistances:
+    yield sd[0]
 
 
 
 def levenshtein(s1, s2):
-  '''Returns the levenshtein distance (aka edit distance) between two strings.'''
+  '''Returns the levenshtein distance (aka edit distance) between two strings.
+
+  Args:
+      s1 (str) - The first string.
+      s2 (str) - The second string.'''
   # Implementation from https://code.activestate.com/recipes/576874-levenshtein-distance/
   # See also, https://en.wikipedia.org/wiki/Levenshtein_distance
   s1_length = len(s1)
--- tests/basicTests.py
+++ tests/basicTests.py
@@ -1,19 +1,231 @@
-import unittest
-import sys
 import os
+import sys
+import unittest
+
 sys.path.insert(0, os.path.abspath('..'))
 import pydidyoumean
 
 runningOnPython2 = sys.version_info[0] == 2
 
+if runningOnPython2:
+    from cStringIO import StringIO
+else:
+    from io import StringIO
+
 class TestGeneral(unittest.TestCase):
+    def test_validateTestFolderContents(self):
+        self.assertEqual(os.listdir(os.path.dirname(os.path.realpath(__file__))),
+                         ['basicTests.py', 'test_file_abc.txt', 'test_file_abcdef.txt', 'test_file_foo.txt'],
+                         'Have you changed the contents on the test folder?')
+
+        self.assertTrue(os.path.exists(os.path.join('..', 'setup.py'))) # the setup.py in the root project folder is also used in these tests.
 
     def test_levenshtein(self):
-        self.assertEqual(pydidyoumean.levenshtein('abc', 'abc'), 0)
-        self.assertEqual(pydidyoumean.levenshtein('abc', 'bc'), 1)
-        self.assertEqual(pydidyoumean.levenshtein('abc', 'ac'), 1)
-        self.assertEqual(pydidyoumean.levenshtein('abc', 'ab'), 1)
-        self.assertEqual(pydidyoumean.levenshtein('abc', 'c'), 2)
+        testData = (('abc', 'abc', 0), # format: (s1, s2, expectedDistance)
+                    ('abc', 'bc', 1),
+                    ('abc', 'ac', 1),
+                    ('abc', 'ab', 1),
+                    ('abc', 'a', 2),
+                    ('abc', 'b', 2),
+                    ('abc', 'c', 2),
+                    ('abc', 'xbc', 1),
+                    ('abc', 'axc', 1),
+                    ('abc', 'abx', 1),
+                    ('abc', 'abcd', 1),
+                    ('It was a bright cold day in April, and the clocks were striking thirteen.',
+                     'It was a bright COld ay in April, xand the clocs were xstriking thrteen.',
+                     7),
+                   )
+
+        for s1, s2, expectedDistance in testData:
+            actualDistance = pydidyoumean.levenshtein(s1, s2)
+            self.assertEqual(expectedDistance, actualDistance,
+                'Expected distance between %r and %r was %s, but actually was %s.' % (s1, s2, expectedDistance, actualDistance))
+
+            # reverse order to check for associative equality
+            actualDistance = pydidyoumean.levenshtein(s2, s1)
+            self.assertEqual(expectedDistance, actualDistance,
+                'Expected distance between %r and %r was %s, but actually was %s.' % (s1, s2, expectedDistance, actualDistance))
+
+
+    def test_printFileSuggestion(self):
+        savedStdOut = sys.stdout
+
+        # basic test
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printFileSuggestion('test_file_ab.txt')
+        self.assertEqual(mystdout.getvalue(), 'Did you mean test_file_abc.txt?\n')
+
+        # test message arg
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printFileSuggestion('test_file_ab.txt', message='How about %s?\n')
+        self.assertEqual(mystdout.getvalue(), 'How about test_file_abc.txt?\n')
+
+        # test folder arg
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printFileSuggestion('test_file_ab.txt', folder=os.path.join('..', 'tests'))
+        self.assertEqual(mystdout.getvalue(), 'Did you mean test_file_abc.txt?\n')
+
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printFileSuggestion('setup.py', folder='..', threshold=0, includeIdenticalFilename=True)
+        self.assertEqual(mystdout.getvalue(), 'Did you mean setup.py?\n')
+
+        # test threshold arg
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printFileSuggestion('test_file_ab.txt', threshold=0)
+        self.assertEqual(mystdout.getvalue(), '')
+
+        # test includeIdenticalFilename arg
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printFileSuggestion('test_file_abc.txt', threshold=0, includeIdenticalFilename=True)
+        self.assertEqual(mystdout.getvalue(), 'Did you mean test_file_abc.txt?\n')
+
+        sys.stdout = savedStdOut
+
+
+    def test_getFileSuggestion(self):
+        # basic test
+        self.assertEqual(pydidyoumean.getFileSuggestion('test_file_ab.txt'),
+                         'Did you mean test_file_abc.txt?\n')
+
+        # test message arg
+        self.assertEqual(pydidyoumean.getFileSuggestion('test_file_ab.txt', message='How about %s?\n'),
+                         'How about test_file_abc.txt?\n')
+
+        # test folder arg
+
+        self.assertEqual(pydidyoumean.getFileSuggestion('test_file_ab.txt', folder=os.path.join('..', 'tests')),
+                         'Did you mean test_file_abc.txt?\n')
+
+
+        self.assertEqual(pydidyoumean.getFileSuggestion('setup.py', folder='..', threshold=0, includeIdenticalFilename=True),
+                         'Did you mean setup.py?\n')
+
+        # test threshold arg
+        self.assertEqual(pydidyoumean.getFileSuggestion('test_file_ab.txt', threshold=0),
+                         '')
+
+        # test includeIdenticalFilename arg
+        self.assertEqual(pydidyoumean.getFileSuggestion('test_file_abc.txt', threshold=0, includeIdenticalFilename=True),
+                        'Did you mean test_file_abc.txt?\n')
+
+
+    def test_suggestFile(self):
+        # basic test
+        self.assertEqual(pydidyoumean.suggestFile('test_file_ab.txt'),
+                         'test_file_abc.txt')
+
+        # test folder arg
+        self.assertEqual(pydidyoumean.suggestFile('test_file_ab.txt', folder=os.path.join('..', 'tests')),
+                         'test_file_abc.txt')
+
+
+        self.assertEqual(pydidyoumean.suggestFile('setup.py', folder='..', threshold=0, includeIdenticalFilename=True),
+                         'setup.py')
+
+        # test threshold arg
+        self.assertEqual(pydidyoumean.suggestFile('test_file_ab.txt', threshold=0),
+                         None)
+
+        # test includeIdenticalFilename arg
+        self.assertEqual(pydidyoumean.suggestFile('test_file_abc.txt', threshold=0, includeIdenticalFilename=True),
+                        'test_file_abc.txt')
+
+
+    def test_suggestAllFiles(self):
+        # basic test
+        self.assertEqual(list(pydidyoumean.suggestAllFiles('test_file_ab.txt')),
+                         ['test_file_abc.txt'])
+
+        # test folder arg
+        self.assertEqual(list(pydidyoumean.suggestAllFiles('test_file_ab.txt', folder=os.path.join('..', 'tests'))),
+                         ['test_file_abc.txt'])
+
+
+        self.assertEqual(list(pydidyoumean.suggestAllFiles('setup.py', folder='..', threshold=0, includeIdenticalFilename=True)),
+                         ['setup.py'])
+
+        # test threshold arg
+        self.assertEqual(list(pydidyoumean.suggestAllFiles('test_file_ab.txt', threshold=0)),
+                         [])
+
+        # test includeIdenticalFilename arg
+        self.assertEqual(list(pydidyoumean.suggestAllFiles('test_file_abc.txt', threshold=0, includeIdenticalFilename=True)),
+                        ['test_file_abc.txt'])
+
+
+    def test_printSuggestion(self):
+        savedStdOut = sys.stdout
+
+        # basic test
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printSuggestion('ab', ['abc', 'abcdef', 'foo'])
+        self.assertEqual(mystdout.getvalue(), 'Did you mean abc?\n')
+
+        # test message arg
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printSuggestion('ab', ['abc', 'abcdef', 'foo'], message='How about %s?\n')
+        self.assertEqual(mystdout.getvalue(), 'How about abc?\n')
+
+        # test threshold arg
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printSuggestion('ab', ['abc', 'abcdef', 'foo'], threshold=0)
+        self.assertEqual(mystdout.getvalue(), '')
+
+        # test includeIdenticalName arg
+        sys.stdout = mystdout = StringIO()
+        pydidyoumean.printSuggestion('abc', ['abc', 'abcdef', 'foo'], threshold=0, includeIdenticalName=True)
+        self.assertEqual(mystdout.getvalue(), 'Did you mean abc?\n')
+
+        sys.stdout = savedStdOut
+
+
+    def test_getSuggestion(self):
+                # basic test
+        self.assertEqual(pydidyoumean.getSuggestion('ab', ['abc', 'abcdef', 'foo']),
+                         'Did you mean abc?\n')
+
+        # test message arg
+        self.assertEqual(pydidyoumean.getSuggestion('ab', ['abc', 'abcdef', 'foo'], message='How about %s?\n'),
+                         'How about abc?\n')
+
+        # test threshold arg
+        self.assertEqual(pydidyoumean.getSuggestion('ab', ['abc', 'abcdef', 'foo'], threshold=0),
+                         '')
+
+        # test includeIdenticalName arg
+        self.assertEqual(pydidyoumean.getSuggestion('abc', ['abc', 'abcdef', 'foo'], threshold=0, includeIdenticalName=True),
+                        'Did you mean abc?\n')
+
+
+    def test_suggest(self):
+        # basic test
+        self.assertEqual(pydidyoumean.suggest('ab', ['abc', 'abcdef', 'foo']),
+                         'abc')
+
+        # test threshold arg
+        self.assertEqual(pydidyoumean.suggest('ab', ['abc', 'abcdef', 'foo'], threshold=0),
+                         None)
+
+        # test includeIdenticalName arg
+        self.assertEqual(pydidyoumean.suggest('abc', ['abc', 'abcdef', 'foo'], threshold=0, includeIdenticalName=True),
+                        'abc')
+
+
+    def test_suggestAll(self):
+        # basic test
+        self.assertEqual(list(pydidyoumean.suggestAll('ab', ['abc', 'abcdef', 'foo'])),
+                         ['abc'])
+
+        # test threshold arg
+        self.assertEqual(list(pydidyoumean.suggestAll('ab', ['abc', 'abcdef', 'foo'], threshold=0)),
+                         [])
+
+        # test includeIdenticalName arg
+        self.assertEqual(list(pydidyoumean.suggestAll('abc', ['abc', 'abcdef', 'foo'], threshold=0, includeIdenticalName=True)),
+                        ['abc'])
+
+
 
 
 if __name__ == '__main__':
