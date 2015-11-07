fe49646fbca6d9e2259557b00fddccc1de9f95d3
Al Sweigart <asweigart@gmail.com>
2015-11-06 23:54:16 -0800

    Update setup.py to pull version info from source code

--- pydidyoumean/__init__.py
+++ pydidyoumean/__init__.py
@@ -240,8 +240,8 @@ def levenshtein(s1, s2):
   matrix = [list(range(s1_length + 1))] * (s2_length + 1)
   for zz in range(s2_length + 1):
     matrix[zz] = list(range(zz,zz + s1_length + 1))
-  for zz in range(0,s2_length):
-    for sz in range(0,s1_length):
+  for zz in range(0, s2_length):
+    for sz in range(0, s1_length):
       if s1[sz] == s2[zz]:
         matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz])
       else:
